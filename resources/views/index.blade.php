4444e01f2343254a6aadc3772ed6a9cfebdf8ab8
satriotol <satriotol69@gmail.com>
2021-02-19 21:41:13 +0700

    index style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -136,24 +136,34 @@
 
                 <div class="col-lg-4 col-md-6">
                     <div class="box">
-
-                        <img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_177b6f517de%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_177b6f517de%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.203125%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
-                            alt="">
+                        <img class="img-fluid" src="https://cdn-2.tstatic.net/tribunjatengwiki/foto/bank/images/rs-roemani-muhammadiyah-semarang.jpg" alt="">
                         <div data-aos="zoom-in" data-aos-delay="100">
-                            <h3>T-shirt</h3>
                             <ul>
+                                <h3>T-shirt</h3>
+                                <li>Cotton Combet 30s</li>
+                            </ul>
+                            <ul>
+                                <h3>Lengan</h3>
+                                <li>Lengan Pendek</li>
+                                <li>Lengan Panjang</li>
+                            </ul>
+                            <ul>
+                                <h3>Sablon</h3>
                                 <li>Aida dere</li>
                                 <li>Nec feugiat nisl</li>
                                 <li>Nulla at volutpat dola</li>
                             </ul>
                             <div class="btn-wrap">
-                                <a href="#" class="btn-buy">Buy Now</a>
+                                <a href="#" class="btn-buy">SIZE CHART</a>
+                            </div>
+                            <div class="btn-wrap">
+                                <a href="#" class="btn-buy">ORDER NOW</a>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
+                <div class="col-lg-4 col-md-6">
                     <div class="box recommended" data-aos="zoom-in">
                         <span class="recommended-badge">Recommended</span>
                         <h3>Business</h3>
@@ -171,7 +181,7 @@
                     </div>
                 </div>
 
-                <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
+                <div class="col-lg-4 col-md-6">
                     <div class="box" data-aos="zoom-in" data-aos-delay="100">
                         <h3>Developer</h3>
                         <h4><sup>$</sup>29<span> / month</span></h4>
