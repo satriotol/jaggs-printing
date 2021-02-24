aa73a60417a69dcec8f277b78184323b4e78957c
satriowindows <satriotol69@gmail.com>
2021-02-24 17:22:54 +0700

    logo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -555,7 +555,7 @@
             <div class="social-links">
                 {{-- <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
                 <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a> --}}
-                <a href="https://www.instagram.com/jaggs.printing/" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="https://www.instagram.com/jaggs.printing/" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
                 {{-- <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
                 <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a> --}}
             </div>
