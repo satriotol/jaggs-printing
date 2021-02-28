73044520e45162f124a6214b4c2e39a079b68cc4
satriotol <satriotol69@gmail.com>
2021-02-28 23:14:26 +0700

    index

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class IndexController extends Controller
 {
     /**
      * Display a listing of the resource.
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::resource('/', IndexController::class);
 // Route::resource('/',IndexController::class);
 
 Auth::routes([
@@ -23,3 +22,5 @@ Auth::routes([
 ]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('/', IndexController::class);
+
